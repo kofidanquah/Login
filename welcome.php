6b93be0ccc51f5978b89700c626ca999eb7fda40
kofidanquah <147538240+kofidanquah@users.noreply.github.com>
2023-10-20 16:26:22 +0000

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,12 @@
+
+<style>
+    body {
+        background-image: url("welcome 2.png");
+        background-repeat: no-repeat;
+        /* background-size: cover; */
+        background-size: 300px;
+        background-position: center;
+    }
+</style>
+
+<?php echo $_POST["name"];?>
